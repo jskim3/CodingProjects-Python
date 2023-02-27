46c6d9e6b2dba734b67564516e5593f8a286b75f
jennykim <26585890+jskim3@users.noreply.github.com>
2023-02-26 19:00:42 -0600

    Updated animal quiz

--- animal_quiz.py
+++ animal_quiz.py
@@ -1,13 +1,23 @@
 # ANIMAL QUIZ
 def check_guess(guess, answer):
     global score
-    if guess.lower() == answer.lower():
-        print('Correct answer')
-        score += 1
-#    else:
-#        guess = input('Sorry, wrong answer. Try again. ')
+    still_guessing = True
+    attempt = 0
+    max_attempts = 1
+    while still_guessing and attempt < max_attempts:            
+        if guess.lower() == answer.lower():
+            print('Correct answer')
+            score += 1
+            still_guessing = False
+        else:
+            attempt +=1
+            if attempt < max_attempts-1:
+                guess = input('Sorry, wrong answer. Try again. ')
+            elif attempt == max_attempts-1:
+                guess = input('Sorry, wrong answer. You only get one more guess. ')
+        if attempt == max_attempts:
+            print('The correct answer is ' + answer)
         
-
 score = 0
 print('Guess the Animal!')
 
@@ -19,5 +29,9 @@ check_guess(guess2, 'cheetah')
 
 guess3 = input('Which is the largest animal?: ')
 check_guess(guess3, 'blue whale')
+
+guess4 = input('Which one of these is a fish? \n \
+A) Whale\n B) Dolphin\n C) Shark\n D) Squid\n (Type A, B, C, or D): ')
+check_guess(guess4, 'C')
 
 print('Your score is ' + str(score))
