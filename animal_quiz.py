709472031e45ea8cef5ce43210cc929c0e8dca45
jennykim <26585890+jskim3@users.noreply.github.com>
2023-02-26 18:13:38 -0600

    Adding in pdf doc and first two projects: hello_world.py and animal_quiz.py

---
+++ animal_quiz.py
@@ -0,0 +1,23 @@
+# ANIMAL QUIZ
+def check_guess(guess, answer):
+    global score
+    if guess.lower() == answer.lower():
+        print('Correct answer')
+        score += 1
+#    else:
+#        guess = input('Sorry, wrong answer. Try again. ')
+        
+
+score = 0
+print('Guess the Animal!')
+
+guess1 = input('Which bear lives at the North Pole?: ')
+check_guess(guess1, 'polar bear')
+
+guess2 = input('Which is the fastest land animal?: ')
+check_guess(guess2, 'cheetah')
+
+guess3 = input('Which is the largest animal?: ')
+check_guess(guess3, 'blue whale')
+
+print('Your score is ' + str(score))
